25b01aa8fab393e3c8be2842a23030a1a9aab175
Adrián Araque <adrarom@gmail.com>
2024-08-26 21:38:17 +0000

    edit for vercel

---
+++ api/app.js
@@ -0,0 +1,47 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const connectDB = require('./config/database');
+const dotenv = require('dotenv');
+const { swaggerUi, specs } = require('./swagger');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+app.use('/api/envelopes', require('./routes/envelopeRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/transactions', require('./routes/transactionsRoutes'))
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
